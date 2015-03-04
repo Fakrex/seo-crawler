7678107e0aec0a5da402934b3cba1dcc313a163a
RadikFakhriev <radik115@gmail.com>
2015-03-04 14:39:37 +0400

    First Commit

---
+++ search.js
@@ -0,0 +1,91 @@
+var webPage = require('webpage'),
+    page = webPage.create(),
+    googleGeo = require('./google-geo'),
+    base64 = require('./base64Processor');
+
+function searchEngine(engConfig) {
+    var _engineUrl = engConfig.rootDomain,
+        _engineRequest = engConfig.requestPrefix,
+        _concatSign = engConfig.concatSign,
+        _engineNewPage = engConfig.newPagePrefix,
+        _linkWrapperSelector = engConfig.linkWrapperSelector,
+        _locSelector = engConfig.locSelector,
+        _localInfo,
+        _searchPagesUrls = [],
+        _resultLinks;
+
+    var pageHandler = function(uri) {
+        page.open(uri, function (status) {
+            if (status === 'success') {
+
+                page.injectJs('./libs/jquery-2.1.3.min.js');
+
+                _resultLinks = page.evaluate(function(_linkWrapperSelector) {
+                    return $(_linkWrapperSelector).map(function () {
+                        return this.href;
+                    }).get();
+                }, _linkWrapperSelector);
+                _localInfo = page.evaluate(function(_locSelector) {
+                    return $(_locSelector).text();
+                }, _locSelector);
+
+                saveResults();
+
+                setTimeout(nextPage, 1000);
+            }
+        });
+    };
+
+    var nextPage = function() {
+        var file = _searchPagesUrls.shift();
+        console.log(file);
+        if(!file) phantom.exit();
+        pageHandler(file);
+    };
+
+    this.runSearch = function(searchObj) {
+        var keyPhrase = searchObj.keyPhrase.replace(new RegExp(' ','g'), _concatSign);
+        var url = _engineUrl + _engineRequest + keyPhrase;
+        for (var numPage = 0; numPage < searchObj.depthSearch; numPage++) {
+            url += _engineNewPage+10*numPage;
+            _searchPagesUrls.push(url);
+            url = url.substr(0,url.indexOf(_engineNewPage));
+        }
+        nextPage();
+    };
+
+    var saveResults = function() {
+        console.log(_resultLinks.join('\n'));
+        console.log(_resultLinks.length);
+        console.log(_localInfo);
+    };
+
+}
+
+var googleOptions = {
+    rootDomain: 'http://google.ru/',
+    requestPrefix: 'search?q=',
+    concatSign: '+',
+    newPagePrefix: '&start=',
+    linkWrapperSelector: 'li.g h3 a',
+    locSelector: '#swml_addr'
+};
+
+var useragent = [];
+useragent.push('Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25');
+useragent.push('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20130406 Firefox/23.0');
+useragent.push('Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36 OPR/27.0.1689.76');
+useragent.push('Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36');
+useragent.push('Mozilla/5.0 (Windows NT 6.3; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0');
+
+page.settings.userAgent = useragent[Math.floor(Math.random() * useragent.length)];
+
+var google = new searchEngine(googleOptions);
+
+var inputParams = {
+    keyPhrase:'сайт под ключ',
+    depthSearch: 1,
+    city: 'Moscow'
+};
+
+google.runSearch(inputParams);
