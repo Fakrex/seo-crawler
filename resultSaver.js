b6bf497b8c29aac32740904a770b965692cfc3f2
RadikFakhriev <radik115@gmail.com>
2015-03-10 23:59:51 +0400

    formatted, correct and incomplete output

--- resultSaver.js
+++ resultSaver.js
@@ -1,52 +1,56 @@
 var fs = require('fs'),
     path = 'output.json';
 
+fs.write(path, '', 'w');
+var now = new Date(),
+    resArr = [],
+    strDate = now.toISOString(),
+    currDate = strDate.substr(0,strDate.indexOf('T')) + ' ' + now.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
 
 var exampleObj = {
     "dateTime": "20115-01-01 18:00",
     "city": "Москва",
     "urlSearch": "http://www.mail.ru/sovety/sovet1/index.html",
-    "rootDomain": "http://www.mail.ru",
-    "name": "ООО Рога и копыта",
     "keys": [
-        {
-            "key": "купить квартиру в москве",
-            "yandex": 1,
-            "google": 10
-        },
-        {
-            "key": "купить хрущевку в москве",
-            "yandex": 4,
-            "google": 5
-        },
-        {
-            "key": "купить хрущевку",
-            "yandex": 3,
-            "google": 2
-        }
-    ],
-    "phones": [
-        "+79272956633",
-        "+79272956633",
-        "+79272956633",
-        "+79272956633"
-    ],
-    "emails": [
-        "email2@yandex.ru",
-        "email2@yandex.ru",
-        "email2@yandex.ru",
-        "email2@yandex.ru"
+
     ]
 };
 
 
 
 function resultSaver(obj) {
+    console.log('опа!');
     console.log(obj.searcher + ', ' + obj.city + ', ' + obj.page + ', ' + obj.key);
     console.log(obj.links.join('\n'));
     console.log(obj.links.length);
 
-    fs.write(path, JSON.stringify(exampleObj), 'w');
+    var tmpObj;
+    for(var i = 0; i < obj.links.length; i++) {
+        var keyItem = {};
+        tmpObj = Object.create(exampleObj);
+        tmpObj.city = obj.city;
+        tmpObj.dateTime = currDate;
+        tmpObj.urlSearch = obj.links[i];
+        tmpObj.keys = [];
+        keyItem['key'] = obj.key;
+        switch (obj.searcher) {
+            case 'yandex':
+                keyItem['yandex'] = i + 1;
+                break;
+            case 'google':
+                keyItem['google'] = i + 1;
+                break;
+            default:
+                throw new Error('Неверное значение!');
+        }
+        tmpObj.keys.push(keyItem);
+        resArr.push(tmpObj);
+    }
+
+
+
+    fs.write(path, JSON.stringify(resArr, null, '\t'), 'a');
+
 }
 
 module.exports = resultSaver;
